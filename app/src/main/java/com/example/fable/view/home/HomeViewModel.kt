d6bd8e922823fd3d7956d38d32f22899d26763f9
Faradaii <kdkfrdy@gmail.com>
2024-11-12 20:37:12 +0800

    feat: update ui

--- app/src/main/java/com/example/fable/view/home/HomeViewModel.kt
+++ app/src/main/java/com/example/fable/view/home/HomeViewModel.kt
@@ -1,7 +1,5 @@
 package com.example.fable.view.home
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.example.fable.data.StoryRepository
 
